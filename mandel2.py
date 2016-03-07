f2793d72e1b194bc917af53b85f7c8e54f13cff5
foustja <foustja@gmail.com>
2016-03-07 07:42:13 -0800

    Update mandel2.py

--- mandel2.py
+++ mandel2.py
@@ -29,7 +29,7 @@ sudo apt-get build-dep python-imaging
 sudo apt-get install libjpeg8 libjpeg62-dev libfreetype6 libfreetype6-dev
 sudo pip install Pillow
 
-(This install Pillow, which replaces PIL.)
+(This installs Pillow, which replaces PIL.)
 
 
 
